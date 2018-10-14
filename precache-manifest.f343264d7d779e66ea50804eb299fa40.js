d5ad760cadecc9fb1a8e587037376978e47af4d8
Sagar Mohite <sagar.mohite@live.com>
2018-10-14 17:33:49 -0400

    Updates

---
+++ precache-manifest.f343264d7d779e66ea50804eb299fa40.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e31a20dc566f89a3db40",
+    "url": "/ifa-amerasia/static/js/runtime~main.e31a20dc.js"
+  },
+  {
+    "revision": "1f218e41025908293fcb",
+    "url": "/ifa-amerasia/static/js/main.1f218e41.chunk.js"
+  },
+  {
+    "revision": "d14de5811036d3532e6a",
+    "url": "/ifa-amerasia/static/js/1.d14de581.chunk.js"
+  },
+  {
+    "revision": "5535e4ef239e5946e2e18b6be7bc8cba",
+    "url": "/ifa-amerasia/index.html"
+  }
+];
