36bf6ce3fa3133ea09a546b789b70051513e26c3
Sagar Mohite <sagar.mohite@live.com>
2018-11-04 15:46:52 -0500

    update typography

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,8 @@
 import {createMuiTheme} from '@material-ui/core'
-import "typeface-crimson-text"
+import 'typeface-crimson-text'
 
 export const theme = createMuiTheme({
   typography: {
-    fontFamily: `"Crimson", "Roboto", "Helvetica", "Arial", sans-serif"`
+    fontSize: 14,
   }
 })
