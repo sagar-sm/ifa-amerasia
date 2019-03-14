7dec187bb2ac29277d97abea133b39318284d8e3
jasonvarone <jason.varone@nyu.edu>
2019-03-14 12:04:09 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/ifa-amerasia/precache-manifest.b354396b981172c2de111815b8302810.js"
+  "/amerasia/precache-manifest.43bfa4867b2dba9e6983f12d46b44019.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ifa-amerasia/index.html", {
+workbox.routing.registerNavigationRoute("/amerasia/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
